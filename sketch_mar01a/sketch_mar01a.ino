f0799a02733f5c1b2f2c4328beec60d11549702c
Victor Hugo Rocha Aguiar Pinto <102185271+Victor84286@users.noreply.github.com>
2023-06-21 17:10:05 -0300

    Add files via upload

--- sketch_mar01a/sketch_mar01a.ino
+++ sketch_mar01a/sketch_mar01a.ino
@@ -1,166 +1,34 @@
-// participantes:
-// Victor Hugo Rocha TA
-// Leonardo Tims TA
-// William Fagundes TA
-// Matheus Herzog TA
-// Gabriel Esposito TA
-// Augusto NT
-
 #include <LiquidCrystal.h>
+#include <math.h>
+#include <Keypad.h>
 
-#define modo 1
-// inicializa a biblioteca
-// com os números dos pinos do arduíno
-const int rs = 2, en = 4, d4 = 5, d5 = 6, d6 = 8, d7 = 9;
+#define modo 2
+
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 
 LiquidCrystal lcd_1(rs, en, d4, d5, d6, d7);
 
-int cont = 0;
+const byte LINHAS = 4; // Linhas do teclado
+const byte COLUNAS = 4; // Colunas do teclado
 
-void complemento_1(int bin[]){
-  
-  for(int i = 0; i<6;i++){
-    if(bin[i] == 0){
-      bin[i] = 1;
-    }
-    else if(bin[i] == 1){
-      bin[i] = 0;
-    }
-  }
-  for(int i = 0; i<6; i++){
-    lcd_1.setCursor(5+i, 1);
-    lcd_1.print(bin[i]);
-  }
-}
+const char TECLAS_MATRIZ[LINHAS][COLUNAS] = { // Matriz de caracteres (mapeamento do teclado)
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
 
-void complemento_2(int bin[]){
-  
-  for(int i = 0; i<6;i++){
-    if(bin[i] == 0){
-      bin[i] = 1;
-    }
-    else if(bin[i] == 1){
-      bin[i] = 0;
-    }
-  }
-  
-  //soma 1
-  bin[5] += 1;
-  //corrige para binario
-  
-  for(int i = 5; i>=0; i--){
-    if(bin[i] == 2){
-      bin[i] = 0;
-      bin[i-1] += 1;
-    }
-  }
-  
-  for(int i = 0; i<6; i++){
-    lcd_1.setCursor(5+i, 1);
-    lcd_1.print(bin[i]);
-  }
-}
+const byte PINOS_LINHAS[LINHAS] = {33, 35, 37, 39}; // Pinos de conexao com as linhas do teclado
+const byte PINOS_COLUNAS[COLUNAS] = {41, 43, 45, 47}; // Pinos de conexao com as colunas do teclado
 
-void soma(int bin[]){
-  int resposta[6];
+Keypad teclado_personalizado = Keypad(makeKeymap(TECLAS_MATRIZ), PINOS_LINHAS, PINOS_COLUNAS, LINHAS, COLUNAS); // Inicia teclado
 
-  for(int i = 0; i<6; i++){
-    resposta[i] = 0;
-  }
-  
-  resposta[5] = bin[5] + bin[2];
-  resposta[4] = bin[4] + bin[1];
-  resposta[3] = bin[3] + bin[0];
-  
-  for(int i = 5; i>=0;i--){
-    if(resposta[i] == 3){
-      resposta[i] = 1;
-      resposta[i-1] += 1;
-    }
-    if(resposta[i] == 2){
-      resposta[i] = 0;
-      resposta[i-1] += 1;
-    }
-  }
-  
-  for(int i = 0; i<6; i++){
-    lcd_1.setCursor(5+i, 1);
-    lcd_1.print(resposta[i]);
-  }
-}
+int X = 0, Y = 0;
+int PALAVRA = 11;
+int COUNT = 0;
 
-void subtracao(int bin[]){
-  int num1 = 0, num2 = 0;
-  int resposta;
-  
-  num1 += bin[0]*4;
-  num1 += bin[1]*2;
-  num1 += bin[2]*1;
-  
-  num2 += bin[3]*4;
-  num2 += bin[4]*2;
-  num2 += bin[5]*1;
-  
-  resposta = num2 - num1;
-  
-  for(int i = 0; i<6; i++){
-    bin[i] = 0;
-  }
-  
-  //resposta >= 0
-  if(resposta >= 0){
-    if(resposta >=4){
-      bin[3] = 1;
-      resposta -= 4;
-    }
-    if(resposta >=2){
-      bin[4] = 1;
-      resposta -= 2;
-    }
-    if(resposta >=1){
-      bin[5] = 1;
-      resposta -= 1;
-    }
-    
-    for(int i = 0; i<6; i++){
-      lcd_1.setCursor(5+i, 1);
-      lcd_1.print(bin[i]);
-    }
-  }
-  
-  // resposta < 0
-  else if (resposta < 0){
-    resposta *= -1;
-    
-    if(resposta >=4){
-      bin[3] = 1;
-      resposta = resposta - 4;
-    }
-    if(resposta >=2){
-      bin[4] = 1;
-      resposta = resposta - 2;
-    }
-    if(resposta >=1){
-      bin[5] = 1;
-      resposta = resposta - 1;
-    }
-    // complemento de 1
-  if(modo == 0){
-      complemento_1(bin);
-    }
-    //complemento de 2
-    else if(modo == 1){
-      complemento_2(bin);
-    }
-    // bit sinal
-    else if(modo == 2){
-      bin[0] = 1;
-      for(int i = 0; i<6; i++){
-        lcd_1.setCursor(5+i, 1);
-        lcd_1.print(bin[i]);
-      }
-    }
-  }
+int f1(void){
+  return 10;
 }
 
 void setup() {
@@ -169,49 +37,258 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  int entrada;
-  int bin[6];
+int add(int bin[]) {
+  int val1 = 0, val2 = 0;
+
+  for(int i = 0; i < 4; i++){
+    val1 += (bin[i+3]*pow(2, 3-i));
+    val2 += (bin[i+7]*pow(2, 3-i));
+
+    if(bin[i+7]*pow(2, 3-i) >= 4){
+      val2+=1;
+    }
+    if(bin[i+3]*pow(2, 3-i) >= 4){
+      val1+=1;
+    }
+  }
+
+  X = val1+val2;
+
+  Serial.print(X);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+int addI(int bin[]){
+  int dec = 0;
+
+  for(int i = 0; i < 8; i++){
+    dec += (bin[i+3]*pow(2, 7-i));
+    
+    if(bin[i+3]*pow(2, 7-i) >= 4){
+      dec += 1;
+    }
+  }
+
+  if(dec > 38) dec -= 1;
+
+  X += dec;
+
+  if(X > 255){
+    lcd_1.setCursor(4, 1);
+    lcd_1.print("Overflow");
+    X = 0;
+  }
+
+  Serial.print(X);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+int comp1(int bin[]){
+  int dec = 0;
+
+
+  for(int i = 0; i < 8; i++){
+    dec += (bin[i+3]*pow(2, 8-i));
+    
+    if(bin[i+3]*pow(2, 7-i) >= 4){
+      dec += 1;
+    }
+  }
+
+  dec *= -1;
+
+  X = 255 - dec;
+
+  Serial.print(X);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+int armazena(int bin[]){
+  int dec = 0;
+
+  for(int i = 0; i < 8; i++){
+    dec += (bin[i+3]*pow(2, 7-i));
+  }
+
+  X = dec;
+
+  Serial.print(X);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+int def_print(int bin[]){
+  int cont = 0;
+
+  for(int i = 3; i<11; i++){
+    if(bin[i] == 1) {
+      cont++;
+      break;
+    }
+  }
+
+  Serial.print("definido \n");
   if(cont == 0){
-    lcd_1.setCursor(4, 0);
-    lcd_1.print("coloque o");
-    lcd_1.setCursor(5, 1); 
-    lcd_1.print("binario");   
+    printX();
   }
-  if(Serial.available() >= 6 && cont == 0){
+  else{
+    printY();
+  }
+
+  return 0;
+}
+
+int printX(){
+  int bin[9];
+  int x = X;
+
+  for(int i = 0;i<9;i++){
+    bin[i] = 0;
+  }
+
+  if(x<0) {
+    x *= -1;
+    bin[0] = 1;
+  }
+
+  for(int i = 1;i<9;i++){
+    if(x >= pow(2, 8-i)){
+      x -= pow(2, 8-i);
+      bin[i] = 1;
+    }
+  }
+
+  for(int i = 0; i<9;i++){
+    lcd_1.setCursor(3+i, 0);
+    lcd_1.print(bin[i]);
+  }
+
+  Serial.print(x);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+int printY(){
+  int bin[9];
+  int y = Y;
+
+  for(int i = 0;i<9;i++){
+    bin[i] = 0;
+  }
+
+  if(y<0) {
+    y *= 0;
+    bin[0] = 1;
+  }
+
+  for(int i = 1;i<9;i++){
+    if(y >= pow(2, 8-i)){
+      y -= pow(2, 8-i);
+      bin[i] = 1;
+    }
+  }
+
+  for(int i = 0; i<9;i++){
+    lcd_1.setCursor(3+i, 0);
+    lcd_1.print(bin[i]);
+  }
+
+  Serial.print(Y);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+int sub(int bin[]){
+  int val1 = 0, val2 = 0;
+
+  for(int i = 0; i < 4; i++){
+    val1 += (bin[i+3]*pow(2, 3-i));
+    val2 += (bin[i+7]*pow(2, 3-i));
+
+    if(bin[i+7]*pow(2, 3-i) >= 4){
+      val2+=1;
+    }
+    if(bin[i+3]*pow(2, 3-i) >= 4){
+      val1+=1;
+    }
+  }
+
+  X = val1-val2;
+
+  Serial.print(X);
+  Serial.print("\n");
+  return 0;
+}
+
+int subI(int bin[]){
+  int dec = 0;
+  for(int i = 3; i <11 ; i++){
+    dec += bin[i]*pow(2, 10-i);
+
+    if(bin[i]*pow(2, 10-i) >= 8){
+      dec += 1;
+    }
+  }
+
+  X -= dec;
+
+  if(X < -255){
+    lcd_1.setCursor(4, 1);
+    lcd_1.print("Overflow");
+    X = 0;
+  }
+
+  Serial.print(X);
+  Serial.print("\n");
+  
+  return 0;
+}
+
+void loop() {
+  char leitura_teclas = teclado_personalizado.getKey();
+  int bin[PALAVRA];
+
+  if (leitura_teclas) {// Se alguma tecla foi pressionada
+    Serial.println(leitura_teclas); // Imprime a tecla pressionada na porta serial
+    Serial.print(COUNT);
+    if(leitura_teclas == '1'){
+      bin[COUNT] = 3;
+      
+
+      lcd_1.print(1);
+    }
+    else{
+      bin[COUNT] = 2;
+      
+      lcd_1.print(0);
+    }
+    COUNT += 1;
+  }
+
+  if(COUNT == 11){
     lcd_1.clear();
-    cont++;
-    
-    for(int i = 0; i<6; i++){
-      bin[i] = Serial.read();
-
-      bin[i] -= 48;
-      lcd_1.setCursor(5+i, 0);
-      lcd_1.print(bin[i]);
+    for(int i = 0; i < 11; i++){
+      bin[i] -= 2;
     }
     
-
-// complemento de 1
-    if(bin[0] == 0 && bin[1] == 0 && cont == 1){
-      cont++;
-      complemento_1(bin);
-    }
-// complemento de 2  
-    if(bin[0] == 0 && bin[1] == 1 && cont == 1){
-      cont++;
-      complemento_2(bin);
-    }
-
-// soma
-    if(bin[0] == 1 && bin[1] == 0 && cont == 1){
-      cont++;
-      soma(bin);
-    }
-
-// subtracao
-    if(bin[0] == 1 && bin[1] == 1 && cont == 1){
-      cont++;
-      subtracao(bin);
-    }
+    if(bin[0] == 0 && bin[1] == 0 && bin[2] == 0) comp1(bin);
+    if(bin[0] == 0 && bin[1] == 0 && bin[2] == 1) add(bin);
+    if(bin[0] == 0 && bin[1] == 1 && bin[2] == 0) addI(bin);
+    if(bin[0] == 0 && bin[1] == 1 && bin[2] == 1) sub(bin);
+    if(bin[0] == 1 && bin[1] == 0 && bin[2] == 0) subI(bin);
+    if(bin[0] == 1 && bin[1] == 0 && bin[2] == 1) def_print(bin);
+    if(bin[0] == 1 && bin[1] == 1 && bin[2] == 0) armazena(bin);
+    if(bin[0] == 1 && bin[1] == 1 && bin[2] == 1) Y = X;
+    COUNT = 0;
   }
+  
 }
